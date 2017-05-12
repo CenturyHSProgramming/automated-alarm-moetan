44ded262430c58d46e7b117264358da02e256899
moe moe <moe moe>
2017-05-12 09:07:59 -0700

    adding successful code

--- automatedAlarm.py
+++ automatedAlarm.py
@@ -5,6 +5,20 @@
 
 # Make sure it returns a value
 
+def automatedAlarm(day,noSchool):
+    if day in('Saturday','Sunday'):
+            return '9:00'
+    elif noSchool==False:
+        if day in('Monday', 'Tuesday','Thursday','Friday'):
+            return '7:00'
+        elif day=='Wednesday':
+            return '7:30'
+    elif noSchool==True:
+        if day== 'Monday':
+            return '9:30'
+        elif day in('Wednesday', 'Tuesday','Thursday','Friday'):
+            return '8:30'
+
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
